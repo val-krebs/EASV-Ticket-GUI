32148b796fcf706381b2a6208af1c162ac55a0e9
hlacis <jakubhlacik@seznam.cz>
2026-02-18 20:35:22 +0100

    Initial commit

---
+++ src/main/java/dk/easv/ticket_gui_only/Launcher.java
@@ -0,0 +1,9 @@
+package dk.easv.ticket_gui_only;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
